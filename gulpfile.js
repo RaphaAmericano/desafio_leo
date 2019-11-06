c2d16869e0cc3e9be2acc3c1a9388703fef152df
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2019-11-06 00:15:52 -0300

    Footer com o grid pronto

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var browser = require('browser-sync').create();
 
 gulp.task('scss', function(){
     return gulp.src('scss/style.scss')
@@ -19,5 +20,13 @@ gulp.task('javascript', function(){
 });
 
 gulp.task('watch', function(){
+    
+    browser.init({
+        server: {
+            baseDir: './'
+        }
+    });
+
     gulp.watch(['scss/*.scss', 'js/*.js'], gulp.series('scss', 'javascript'));
+    gulp.watch(['*.html','scss/*.scss', 'js/*.js']).on('change', browser.reload);
 });
