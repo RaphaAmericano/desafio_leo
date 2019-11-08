588a67aebe39849d20f3aeb23c794dcc426b15a0
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2019-11-08 00:08:10 -0300

    Slider funcionando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,39 @@
 (function(){
-    console.log('Hello, World');
-}());
+    var $slides = document.getElementsByClassName('slide');
+    var $arrowLeft = document.getElementsByClassName('left_arrow')[0];
+    var $arrowRight = document.getElementsByClassName('right_arrow')[0];
+    
+
+    $arrowLeft.addEventListener('click', function(e){
+        e.preventDefault();
+        for(var i = 0; i < $slides.length; i++){ 
+            if($slides[i].classList.contains('active')){
+                slideIndex = i;
+                break;
+            }
+        }
+        $slides[slideIndex].classList.remove('active');
+        if(slideIndex == 0){    
+            $slides[$slides.length -1 ].classList.add('active');
+        } else {
+            $slides[slideIndex - 1].classList.add('active');
+        }
+    });
+
+    $arrowRight.addEventListener('click', function(e){
+        e.preventDefault();
+        for(var i = 0; i < $slides.length; i++){ 
+            if($slides[i].classList.contains('active')){
+                slideIndex = i;
+                break;
+            }
+        }
+        $slides[slideIndex].classList.remove('active');
+        if(slideIndex == $slides.length -1){
+            $slides[0].classList.add('active');
+        } else {
+            $slides[slideIndex + 1].classList.add('active');
+        }
+    });
+
+})();
